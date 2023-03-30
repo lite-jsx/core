4a985b9d82ab20c134e4be334e14d1a70935c52f
Daniel Prates <danprates94@gmail.com>
2023-03-30 18:54:19 -0300

    test(generateTag): ensure render correct tag when valid params are provided

--- tests/generate-tag.spec.js
+++ tests/generate-tag.spec.js
@@ -5,8 +5,16 @@ export default function () {
   // should render DOCTYPE when html tag is provided
   {
     strictEqual(
-      generateTag("html", 'class="my-class"', "<title>generateTag</title>"),
-      '<!DOCTYPE html>\n<html class="my-class"><title>generateTag</title></html>'
+      generateTag("html", 'class="my-class"', "<title>hello</title>"),
+      '<!DOCTYPE html>\n<html class="my-class"><title>hello</title></html>'
+    );
+  }
+
+  // should render correct tag when valid params are provided
+  {
+    strictEqual(
+      generateTag("h1", 'class="my-class"', "hello"),
+      '<h1 class="my-class">hello</h1>'
     );
   }
 }
