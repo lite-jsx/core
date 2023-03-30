30e3b62ccf256eaaa50e26cdf369bd4542547945
Daniel Prates <danprates94@gmail.com>
2023-03-30 15:25:03 -0300

    refactor(stringifyChildren): move to its own file

---
+++ src/stringify-childen.js
@@ -0,0 +1,21 @@
+export const stringifyChildren = (child) => {
+  let stringifiedChild = typeof child === "string" ? child : "";
+  if (!stringifiedChild) {
+    if (child instanceof String) {
+      stringifiedChild = child.toString();
+    }
+    if (Array.isArray(child)) {
+      stringifiedChild = child.join("");
+    } else if (child && typeof child.toString === "function") {
+      // there's a value that exposes a `toString` coercion method
+      stringifiedChild = child.toString();
+    }
+
+    // coerce a number of falsey varietes to a string, but exclude null,
+    // which is the standard behavior of React's JSX engine
+    if (!child && child !== null) {
+      stringifiedChild = `${child}`;
+    }
+  }
+  return stringifiedChild;
+};
