1cddceb1ee2ccb2273fe82639578080242f8184a
Daniel Prates <danprates94@gmail.com>
2023-04-11 20:41:04 -0300

    feat(render): add nest render decorator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { h } = require("./h");
 const { expressMiddleware } = require("./express.middleware");
+const { Render } = require("./render.decorator");
 
 module.exports = {
   h,
   __express: expressMiddleware,
+  Render,
 };
